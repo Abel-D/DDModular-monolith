cdd448917c09a9dd49a9f818fc383ce654735367
Abel D <70660536+Abel-D@users.noreply.github.com>
2023-01-04 18:36:53 +0300

    fix

--- src/Modules/Project/Excellerent.Standard.Advanced.Project.Api/Controllers/ProjectController.cs
+++ src/Modules/Project/Excellerent.Standard.Advanced.Project.Api/Controllers/ProjectController.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Excellerent.Standard.Advanced.Project.Core.Queries.GetProjects;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +9,22 @@ using System.Threading.Tasks;
 
 namespace Excellerent.Standard.Advanced.Project.Api.Controllers
 {
-    internal class ProjectController
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController:ControllerBase
     {
         private readonly IMediator _mediator;
 
         public ProjectController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProjects(GetProjectsRequest request)
+        {
+            var projects = await _mediator.Send(new GetProjectsQuery(request));
+
+            return Ok(projects);
         }
     }
 }
---
+++ src/Modules/Project/Excellerent.Standard.Advanced.Project.Core/Queries/GetProjects/GetProjectsQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excellerent.Standard.Advanced.Project.Core.Queries.GetProjects
+{
+    public class GetProjectsQuery
+    {
+        public GetProjectsRequest Request { get; set; }
+        public GetProjectsQuery(GetProjectsRequest request)
+        {
+            Request = request;
+        }
+    }
+}
