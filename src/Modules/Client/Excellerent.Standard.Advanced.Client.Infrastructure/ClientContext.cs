bc218b00158aac0ec85d349fac9b0640b8efd01d
Abel-D <abeldebebe818@gmail.com>
2023-01-05 18:38:42 +0300

    initial migration added

--- src/Modules/Client/Excellerent.Standard.Advanced.Client.Infrastructure/ClientContext.cs
+++ src/Modules/Client/Excellerent.Standard.Advanced.Client.Infrastructure/ClientContext.cs
@@ -1,6 +1,8 @@
-﻿using Excellerent.Standard.Advanced.Shared.Database;
+﻿using Excellerent.Standard.Advanced.Client.Infrastructure;
+using Excellerent.Standard.Advanced.Shared.Database;
 using Excellerent.Standard.Advanced.Shared.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +17,20 @@ namespace Excellerent.Standard.Advanced.Client.Infrastructure
         {
         }
         public DbSet<ClientEntity> Clients { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=3002;Server=localhost;Port=5432;Database=Client;Pooling=true;");
+        }
+
+        internal class ClientContextFactory : IDesignTimeDbContextFactory<ClientContext>
+        {
+            public ClientContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<ClientContext>();
+
+                return new ClientContext(optionsBuilder.Options);
+            }
+        }
     }
+    
 }
--- src/Modules/Client/Excellerent.Standard.Advanced.Client.Infrastructure/Extensions.cs
+++ src/Modules/Client/Excellerent.Standard.Advanced.Client.Infrastructure/Extensions.cs
@@ -1,14 +1,7 @@
 ﻿
 using Excellerent.Standard.Advanced.Client.Core.Contracts;
-using Excellerent.Standard.Advanced.Shared.Database;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Excellerent.Standard.Advanced.Client.Infrastructure
 {
@@ -16,8 +9,7 @@ namespace Excellerent.Standard.Advanced.Client.Infrastructure
     {
         public static IServiceCollection AddClientInfrastructureServices(this IServiceCollection services)
         {
-            services.AddDbContext<ClientContext>(options => options.UseNpgsql("User ID=postgres;Password=Password;Server=localhost;Port=5432;Databasennk=Client;Pooling=true;"));
-            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IClientRepository,ClientRepository>();
             return services;
         }
     }
